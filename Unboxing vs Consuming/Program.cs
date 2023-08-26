99fd6dd921e3ebfc51ea93979b80526b36c81387
Kulpot <kulpotwahu@gmail.com>
2023-08-26 19:18:16 +0800

    Add project files.

---
+++ Unboxing vs Consuming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=8HnXXOLb-TQ&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=13
+//  MSIL(MS intermidiate langauge) - boxing/unboxing
+
+class MainClass
+{
+    static void Main()
+    {
+        object o = 5;
+        //((int)o);   // just boxing/unboxing
+        //int i = ((int)o);   // consume box
+        o.ToString();   //  same as "((int)o).ToString();"
+        ((int)o).ToString();    // just boxing/unboxing
+    }
+}
